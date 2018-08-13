b37c24516ecc4d9703dcf1a16a57fb6c341d4036
seanchrist <seanchristspc@gmail.com>
2018-08-13 11:06:01 +0800

    文件过滤显示

---
+++ src/com/sean/unit18/DirList.java
@@ -0,0 +1,38 @@
+package com.sean.unit18;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class DirList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File path=new File(".");
+		String[] list;
+		if(args.length==0)
+			list=path.list();
+		else
+			list=path.list(new DirFilter(args[0]));
+		
+		Arrays.sort(list,String.CASE_INSENSITIVE_ORDER);
+		for(String dirItem:list)
+			System.out.println(dirItem);
+	}
+
+}
+
+
+class  DirFilter implements FilenameFilter{
+	private Pattern pattern;
+	public DirFilter(String regex) {
+		pattern=Pattern.compile(regex);
+	}
+	@Override
+	public boolean accept(File dir, String name) {
+		// TODO Auto-generated method stub
+		return pattern.matcher(name).matches();
+	}
+	
+}
